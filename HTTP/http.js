cc6a2a8ac184378214748a5c74ec6863fcec1f1f
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-11-27 08:49:00 -0300

    att

---
+++ HTTP/http.js
@@ -0,0 +1,10 @@
+var http = require('http')//importando o metodo http
+
+http.createServer(function (req, res) {
+    res.end('Hello World, this my first server')
+}).listen(8081)
+
+console.log("Server Running")
+//CreateServer è uma Função para Criar O servidor que recebe 2 paramentros req=requisição res=resposta
+//end() é para enviar uma mensagem
+//listen define a porta na qual seu servidor irá abrir
