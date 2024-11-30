f722bd27adf8abe75fd3b59a90817036a77df1d0
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-11-30 14:04:33 -0300

    att

--- form/form.js
+++ form/form.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const { engine } = require('express-handlebars');  // Importação correta
+const { engine } = require('express-handlebars');
 const Sequelize = require('sequelize');
 
 // Template Engine
 app.engine('handlebars', engine({ defaultLayout: 'main' }));  // Usando o objeto engine
-app.set('view engine', 'handlebars');  // Remove o espaço extra antes de 'handlebars'
+app.set('view engine', 'handlebars');
 
 // Conexão com o banco de dados
 const sequelize = new Sequelize('sistemadecadastro', 'root', '1234', {
@@ -15,10 +15,16 @@ const sequelize = new Sequelize('sistemadecadastro', 'root', '1234', {
 
 // Rotas
 app.get('/cad', (req, res) => {
-    res.render('form');
+    res.render('form');// redenrizar o arquivo
 });
+//Envia o Form
+app.post('/add', (req, res) => {
+    res.send('Form recebido!!')
+})
 
 // Inicia o servidor
 app.listen(8081, () => {
-    console.log('Server Running at URL http://localhost:8081');  // Corrigido o protocolo https para http
+    console.log('Server Running at URL http://localhost:8081');  //
 });
+
+
