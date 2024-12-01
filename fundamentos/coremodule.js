012b0690d123b6d2c3780913622fac2ca69bbefd
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-01 14:35:18 -0300

    att

---
+++ fundamentos/coremodule.js
@@ -0,0 +1,3 @@
+const path = require('path')
+const extension = path.extname("arq.tjghjghs")
+console.log(extension)
--- require/somar.js
+++
@@ -1,2 +0,0 @@
-var app = require("./calc")//faz a chamada deste modulo
-console.log(app(1, 2))
