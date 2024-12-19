ffdc85cc2a928afceb9fd11bbfdea7f4d3bb83e1
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-18 22:05:06 -0300

    att

--- Tarefas/produto/router.js
+++ Tarefas/produto/router.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const Path = require('path')
-const BasePath = Path.resolve(__dirname,'../template')
+const BasePath = Path.join(__dirname,'../template')
 const fs = require('fs')
 
 router.get('/add',(req,res)=>{
     res.sendFile(`${BasePath}/add.html`)
 })
-
 router.post('/save',(req,res)=>{
     if(!fs.existsSync('Estoque')){
         fs.mkdirSync('Estoque')
@@ -15,7 +14,7 @@ router.post('/save',(req,res)=>{
     const Produto = req.body.produto
     const Quant = req.body.qntProduto
 
-    fs.writeFileSync(`Estoque/${Produto}.json`, JSON.stringify({name:Produto,quant:Quant}))
+    fs.writeFileSync(`Estoque/${Produto}.json`, JSON.stringify({name:Produto,quant:Quant}),(e)=>{console.log(e)})
 
     res.sendFile(`${BasePath}/add.html`)
 })
--- Template Engine/index.js
+++ Template Engine/index.js
@@ -1,14 +1,40 @@
+// Importa o módulo Express para criar o servidor
 const express = require('express')
+
+// Importa o módulo express-handlebars para renderizar templates
 const exphbs = require('express-handlebars')
+
+// Inicializa o aplicativo Express
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+// Configura o motor de visualização (template engine) como handlebars
+app.engine('handlebars', exphbs.engine()) // Define o 'engine' do express como o handlebars
+app.set('view engine', 'handlebars') // Define o 'view engine' para usar arquivos .handlebars
 
-app.get('/',(req,res)=>{
-    res.render('home', {layout:false})
+// Define uma rota GET para "/dashboard"
+app.get('/dashboard', (req, res) => {
+    // Renderiza a página "dashboard.handlebars"
+    res.render('dashboard')
 })
 
-app.listen(3000,()=>{
+// Define uma rota GET para a raiz "/"
+app.get('/', (req, res) => {
+    // Cria um objeto User com informações fictícias
+    const User = {
+        name: 'Vitor', // Nome do usuário
+        age: 16,       // Idade do usuário
+        city: 'icapui-ce' // Cidade do usuário
+    }
+    
+    // Define uma variável de autenticação
+    const auth = false // Indica que o usuário está autenticado
+
+    // Renderiza a página "home.handlebars" com os dados do usuário e a autenticação
+    res.render("home", { user: User, auth })
+})
+
+// Configura o servidor para escutar na porta 5000
+app.listen(5000, () => {
+    // Mensagem de confirmação no terminal quando o servidor está ativo
     console.log('Servidor Rodando')
 })
