b37d8441ceb97c4125c0d44c25190fec272a43c2
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-30 16:17:58 -0300

    att

---
+++ MVC/db/conn.js
@@ -0,0 +1,15 @@
+const {Sequelize}= require("sequelize")
+
+const sequelize = new Sequelize('nodemvc','root','',{
+    host:'localhost',
+    port:3306,
+    dialect:'mysql'
+})
+
+try{
+    console.log('Conexão Feita')
+}catch(err){
+    console.log('erro ao conectar ao banco de dados')
+}
+
+module.exports = sequelize
