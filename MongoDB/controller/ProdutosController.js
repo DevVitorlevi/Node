6562e1604c55be75064a144c9a0e4898a1527ac4
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-11 18:03:07 -0300

    resgatando produto

--- MongoDB/controller/ProdutosController.js
+++ MongoDB/controller/ProdutosController.js
@@ -2,15 +2,18 @@ const Produto = require('../models/Produto')
 
 module.exports = class ProdutosController {
     static async todosProdutos(req,res){
-        res.render('produtos/home')
+
+        const produtos = await Produto.getProdutos()
+
+        res.render('produtos/home', {produtos})
     }
     static criarProduto(req,res){
         res.render('produtos/criar')
     }
     static async adicionarProduto(req,res){
-        const {nome,preco,descricao} = req.body
+        const {nome,image,preco,descricao} = req.body
 
-        const produto = new Produto(nome,preco,descricao)
+        const produto = new Produto(nome,image,preco,descricao)
 
         produto.save()
 
--- MongoDB/models/Produto.js
+++ MongoDB/models/Produto.js
@@ -1,8 +1,9 @@
 const conn = require('../db/conn')
 
 class Produto {
-    constructor(nome,preco,descricao){
+    constructor(nome,image,preco,descricao){
         this.nome = nome
+        this.image= image
         this.preco = preco
         this.descricao = descricao
     }
@@ -10,10 +11,17 @@ class Produto {
     save(){
         const produto = conn.db().collection('produtos').insertOne({
             nome:this.nome,
+            image:this.image,
             preco:this.preco,
             descricao:this.descricao
         })
         return produto
+    }
+
+    static getProdutos(){
+        const produtos = conn.db().collection('produtos').find().toArray()
+
+        return produtos
     }
 }
 
