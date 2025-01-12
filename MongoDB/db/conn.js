52bf70f5b3816034b90b23cf1d5400dcc5da1526
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-12 18:24:57 -0300

    instalar mongoose

--- MongoDB/db/conn.js
+++ MongoDB/db/conn.js
@@ -1,20 +1,11 @@
-const {MongoClient}= require('mongodb')
+const mongoose = require('mongoose')
 
-const uri = "mongodb://127.0.0.1:27017/mongodbnode";
-
-
-const client = new MongoClient(uri)
-
-async function run() {
-    try{
-        await client.connect()
-        console.log('Conexão Feita com sucesso')
-    }
-    catch(err){
-        console.log('Conexão Fracassada',err)
-    }
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mongodbnode")
+    console.log('Conectado Com Sucesso')
 }
 
-run()
+main().catch(err => console.log(err))
 
-module.exports = client
+
+module.exports = mongoose
